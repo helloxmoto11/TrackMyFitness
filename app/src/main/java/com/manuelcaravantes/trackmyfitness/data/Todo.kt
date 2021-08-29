d6f259befb55c3fc4dff45c5d5c011984a003427
MannyDesktop <rhcplive11@gmail.com>
2021-08-29 15:13:51 -0500

    added Todo.kt

---
+++ app/src/main/java/com/manuelcaravantes/trackmyfitness/data/Todo.kt
@@ -0,0 +1,13 @@
+package com.manuelcaravantes.trackmyfitness.data
+
+/**
+ *
+ * A todo list to keep me on track.
+ */
+// TODO: 8/29/2021 convert Main Column into Lazy Column to take any number of Cards.
+// TODO: 8/29/2021 update Activity/Exercise Schema
+// TODO: 8/29/2021 create Activity/Exercise detail screen.
+// TODO: 8/29/2021 refactor add screen
+// TODO: 8/29/2021 work on Drawer
+// TODO: 8/29/2021 Tie in room database
+// TODO: 8/29/2021 create notifications
