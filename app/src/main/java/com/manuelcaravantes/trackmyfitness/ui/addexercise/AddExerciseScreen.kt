242cc2c532d5e9f4413c72670dd4530abeaa9750
MannyDesktop <rhcplive11@gmail.com>
2021-09-13 17:10:28 -0500

    Working on WorkoutCard.kt

--- app/src/main/java/com/manuelcaravantes/trackmyfitness/ui/addexercise/AddExerciseScreen.kt
+++ app/src/main/java/com/manuelcaravantes/trackmyfitness/ui/addexercise/AddExerciseScreen.kt
@@ -27,7 +27,6 @@ import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.manuelcaravantes.trackmyfitness.data.model.fitnessActivityList
 import com.manuelcaravantes.trackmyfitness.ui.addexercise.ExerciseFields.*
-import com.manuelcaravantes.trackmyfitness.ui.theme.TrackMyFitnessTheme
 
 
 @Composable
